0eb88885ee9edb404265e371e735599723408751
Abhinav Gangrade <48849486+abhinav0606@users.noreply.github.com>
2020-07-21 20:05:01 +0530

    Update model.py

--- tech_blog/tech_blog/model.py
+++ tech_blog/tech_blog/model.py
@@ -1,4 +1,13 @@
 from django.db import models
 class registration(models.Model):
-    name=models.CharField(max_length=20)
+    name=models.CharField(max_length=20,default="")
+    email=models.CharField(max_length=30,default="")
+    mobile=models.CharField(max_length=20,default="")
+    username=models.CharField(max_length=30,default="")
+    password=models.CharField(max_length=40,default="")
+    gender=models.CharField(max_length=10,default="")
+    type_user=models.CharField(max_length=10,default="")
 
+
+    def __str__(self):
+        return self.username
