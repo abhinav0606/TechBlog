e8e7c6d1fdf6bc4cbb5a04a18240f21f7a927544
Abhinav Gangrade <48849486+abhinav0606@users.noreply.github.com>
2020-07-28 19:40:39 +0530

    Update model.py

--- tech_blog/tech_blog/model.py
+++ tech_blog/tech_blog/model.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 class registration(models.Model):
     name=models.CharField(max_length=20,default="")
     email=models.CharField(max_length=30,default="")
@@ -11,3 +12,14 @@ class registration(models.Model):
 
     def __str__(self):
         return self.username
+class blog(models.Model):
+    title=models.CharField(max_length=200,default="")
+    language=models.CharField(max_length=20,default="")
+    description=models.CharField(max_length=500,default="")
+    main=models.CharField(max_length=1000,default="")
+    code=models.CharField(max_length=300,default="")
+    output=models.CharField(max_length=300,default="")
+    date=models.CharField(max_length=10,default=str(datetime.datetime.now()).split(" ")[0])
+
+    def __str__(self):
+        return self.title
