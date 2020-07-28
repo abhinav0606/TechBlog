17ab11ff594c4d5ef7cd9ee775b85dfa0f20ffa9
Abhinav Gangrade <48849486+abhinav0606@users.noreply.github.com>
2020-07-28 18:57:59 +0530

    Update view.py

--- tech_blog/tech_blog/view.py
+++ tech_blog/tech_blog/view.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse,request
 from .model import registration
 import smtplib
+import xlsxwriter
 def lgn_rgstr(request):
     l = []
     login = {}
@@ -35,21 +36,28 @@ def lgn_rgstr(request):
             elif password!=passwordc:
                 return render(request,'lgn_rgstr.html',{'message':'The Password Doesnot matched May be some some typing error'})
             else:
-                if genderm=='male':
-                    registration(name=name,mobile=mobile,email=email,username=username,password=password,gender='Male',type_user=type).save()
-                if genderf=='female':
-                    registration(name=name, mobile=mobile, email=email, username=username, password=password, gender='Female',type_user=type).save()
-                if genderp=='other':
-                    registration(name=name, mobile=mobile, email=email, username=username, password=password, gender='Other',type_user=type).save()
-                server=smtplib.SMTP('smtp.gmail.com','587')
-                server.ehlo()
-                server.starttls()
-                Subject="Thanks For Registering"
-                server.login("weatherappabhinavcreations@gmail.com", "weatherapp@abhinavcreations")
-                Message=f"Thanks {name} for registering into TechBlogs.We will notify you regarding any updates \n\n\n\n Regards \n Team TechBlogs"
-                msg='Subject:{}\n\n{}'.format(Subject,Message)
-                server.sendmail("weatherappabhinavcreations@gmail.com",email,msg)
-                server.close()
+                try:
+                        server = smtplib.SMTP('smtp.gmail.com', '587')
+                        server.ehlo()
+                        server.starttls()
+                        Subject = "Thanks For Registering"
+                        server.login("weatherappabhinavcreations@gmail.com", "weatherapp@abhinavcreations")
+                        Message = f"Thanks {name} for registering into TechBlogs.We will notify you regarding any updates \n\n\n\n Regards \n Team TechBlogs"
+                        msg = 'Subject:{}\n\n{}'.format(Subject, Message)
+                        server.sendmail("weatherappabhinavcreations@gmail.com", email, msg)
+                        server.close()
+                        if genderm == 'male':
+                            registration(name=name, mobile=mobile, email=email, username=username, password=password,
+                                         gender='Male', type_user=type).save()
+                        if genderf == 'female':
+                            registration(name=name, mobile=mobile, email=email, username=username, password=password,
+                                         gender='Female', type_user=type).save()
+                        if genderp == 'other':
+                            registration(name=name, mobile=mobile, email=email, username=username, password=password,
+                                         gender='Other', type_user=type).save()
+
+                except:
+                        return render(request,'lgn_rgstr.html',{'message':"Please Enter a valid mail"})
         if username_lgn!='default':
             if username_lgn in l:
                 if password_lgn==login[username_lgn][0]:
@@ -62,3 +70,24 @@ def lgn_rgstr(request):
             else:
                 return render(request,'lgn_rgstr.html',{'message':'This User Doesnot Exist Please Create Account First'})
     return render(request,'lgn_rgstr.html',{'message':''})
+
+def excell(request):
+    Register_login=xlsxwriter.Workbook("Registration.xlsx")
+    excell=Register_login.add_worksheet()
+    excell.set_column(0, 5, 30)
+    excell.write(0,0,"Name")
+    excell.write(0,1,'Email')
+    excell.write(0,2,"Mobile")
+    excell.write(0,3,"Username")
+    excell.write(0,4,'Gender')
+    excell.write(0,5,'Type')
+    list=registration.objects.all()
+    for i in range(len(list)):
+        excell.write(i+1, 0, list[i].name)
+        excell.write(i+1, 1, list[i].email)
+        excell.write(i+1, 2, list[i].mobile)
+        excell.write(i+1, 3, list[i].username)
+        excell.write(i+1, 4, list[i].gender)
+        excell.write(i+1, 5, list[i].type_user)
+    Register_login.close()
+    return HttpResponse("Updated the Excell Sheet")
