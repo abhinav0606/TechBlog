ba413095d0fc775f6685207a8dc4c5a051d12b17
Abhinav Gangrade <48849486+abhinav0606@users.noreply.github.com>
2020-08-14 15:25:07 +0530

    Update admin.py

--- tech_blog/tech_blog/admin.py
+++ tech_blog/tech_blog/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .model import registration,blog
+from .model import registration,blog,writer
 admin.site.register(registration)
 admin.site.register(blog)
+admin.site.register(writer)
